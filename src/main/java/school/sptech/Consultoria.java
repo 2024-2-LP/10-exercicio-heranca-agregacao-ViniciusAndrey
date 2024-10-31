c59415fc046b87c4c633b22258edfdfc3f81bdf0
VinyApenas <vinicius.carvalho@sptech.school>
2024-10-31 12:57:23 -0300

    Lição feita

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,135 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    // Construtor
+
+    public Consultoria() {
+        desenvolvedores = new ArrayList<>();
+    }
+
+    public Consultoria(String nome, Integer vagas) {
+        this.nome = nome;
+        this.vagas = vagas;
+        this.desenvolvedores = new ArrayList<Desenvolvedor>();
+    }
+
+    // Mêtodos
+
+    public Boolean contratar(Desenvolvedor desenvolvedor) {
+            if(desenvolvedores.size() >= vagas) {
+                return false;
+            }
+            desenvolvedores.add(desenvolvedor);
+            return true;
+    }
+
+    public Boolean contratarFullstack(DesenvolvedorWeb desenvolvedor) {
+        if (desenvolvedor != null && desenvolvedor.isFullstack()) {
+            desenvolvedores.add(desenvolvedor);
+            return true;
+        }
+        return false;
+
+    }
+
+    public Double getTotalSalarios() {
+        double salario = 0.0;
+        for (Desenvolvedor desenvolvedor : desenvolvedores) {
+            salario += desenvolvedor.calcularSalario();
+        }
+        return salario;
+    }
+
+    public Integer qtdDesenvolvedoresMobile() {
+        Integer contador = 0;
+        for (Desenvolvedor desenvolvedorMobile : desenvolvedores) {
+            if(desenvolvedorMobile instanceof DesenvolvedorMobile) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario) {
+        List<Desenvolvedor> resultado = new ArrayList<>();
+        for (Desenvolvedor desenvolvedor : desenvolvedores) {
+            if (desenvolvedor.calcularSalario() >= salario) {
+                resultado.add(desenvolvedor);
+            }
+        }
+        return resultado;
+    }
+
+    public Desenvolvedor buscarMenorSalario() {
+        if (desenvolvedores.isEmpty()) {
+            return null;
+        }
+
+        Desenvolvedor menorSalarioDev = desenvolvedores.get(0);
+        for (Desenvolvedor desenvolvedor : desenvolvedores) {
+            if (desenvolvedor.calcularSalario() < menorSalarioDev.calcularSalario()) {
+                menorSalarioDev = desenvolvedor;
+            }
+        }
+        return menorSalarioDev;
+    }
+
+    public List<Desenvolvedor> buscarPorTecnologia(String tecnologia) {
+
+        List<Desenvolvedor> resultadoTec = new ArrayList<>();
+
+        for (Desenvolvedor desenvolvedor : desenvolvedores) {
+            if(desenvolvedor instanceof DesenvolvedorMobile) {
+                if(((DesenvolvedorMobile) desenvolvedor).getPlataforma().equalsIgnoreCase(tecnologia) ||
+                        ((DesenvolvedorMobile) desenvolvedor).getLinguagem().equalsIgnoreCase(tecnologia)) {
+                    resultadoTec.add(desenvolvedor);
+                }
+            }
+            if (desenvolvedor instanceof DesenvolvedorWeb) {
+               if(((DesenvolvedorWeb) desenvolvedor).getBackend().equalsIgnoreCase(tecnologia) ||
+               ((DesenvolvedorWeb) desenvolvedor).getFrontend().equalsIgnoreCase(tecnologia) ||
+               ((DesenvolvedorWeb) desenvolvedor).getSgbd().equalsIgnoreCase(tecnologia)) {
+                   resultadoTec.add(desenvolvedor);
+               }
+            }
+        }
+        return resultadoTec;
+    }
+
+    public Double getTotalSalariosPorTecnologia(String tecnologia) {
+            double salario = 0.0;
+            List<Desenvolvedor> salarioTec = buscarPorTecnologia(tecnologia);
+            for (Desenvolvedor desenvolvedor : salarioTec) {
+                salario += desenvolvedor.calcularSalario();
+            }
+            return salario;
+    }
+
+    // Getter & Setter
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,77 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+    private String backend;
+    private String frontend;
+    private String sgbd;
+    private Integer horasMentoria;
+
+
+    // Construtor
+
+    public DesenvolvedorWeb() {
+    }
+
+    public DesenvolvedorWeb(String nome, Integer qtdHoras, Double valorHora, String backend, String frontend, String sgbd, Integer horasMentoria) {
+        super(nome, qtdHoras, valorHora);
+        this.backend = backend;
+        this.frontend = frontend;
+        this.sgbd = sgbd;
+        this.horasMentoria = horasMentoria;
+    }
+
+
+    // Mêtodos
+
+
+    @Override
+    public Double calcularSalario() {
+        return super.qtdHoras * super.valorHora + (horasMentoria * 300);
+    }
+
+    public Boolean isFullstack() {
+
+        if(backend == null || frontend == null || sgbd == null){
+            return false;
+        }
+        return true;
+    }
+
+
+    // Getter & Setter
+
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+
+    public String getFrontend() {
+        return frontend;
+    }
+
+    public void setFrontend(String frontend) {
+        this.frontend = frontend;
+    }
+
+    public String getSgbd() {
+        return sgbd;
+    }
+
+    public void setSgbd(String sgbd) {
+        this.sgbd = sgbd;
+    }
+
+    public Integer getHorasMentoria() {
+        return horasMentoria;
+    }
+
+    public void setHorasMentoria(Integer horasMentoria) {
+        this.horasMentoria = horasMentoria;
+    }
 }
